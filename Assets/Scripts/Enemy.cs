9670478eebf16ac51862b2f1f6aa43e9a0b56f75
Salmon <sos2012happy@gmail.com>
2022-02-03 16:55:53 +0800

    init

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FrameworkDesign.Example
+{
+public class Enemy : MonoBehaviour
+{
+     private void OnMouseDown() {
+        Destroy(gameObject);
+        }
+    }    
+}
+
